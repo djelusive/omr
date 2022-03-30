8f3dea670da3d51bc494bfbac69a7a3f185c7d7f
djelusive <timothy.kent83@gmail.com>
2022-03-31 08:10:47 +1100

    Update TextTargetTest.php

--- tests/Targets/TextTargetTest.php
+++ tests/Targets/TextTargetTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace JansenFelipe\OMR\Tests;
+namespace Djelusive\OMR\Tests;
 
-use JansenFelipe\OMR\Point;
-use JansenFelipe\OMR\Targets\TextTarget;
+use Djelusive\OMR\Point;
+use Djelusive\OMR\Targets\TextTarget;
 use PHPUnit\Framework\TestCase;
 
 class TextTargetTest extends TestCase
